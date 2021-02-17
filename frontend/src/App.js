e1891e436276d3bb6c0372b2d69db64e9cdcc8d6
benedict <kiokobenedict@gmail.com>
2021-02-18 00:10:56 +0300

    added an add task component with form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { client } from "./apollo-client";
 import { Task } from "./Task";
+import { AddTask } from "./AddTask";
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <div>
+        <AddTask />
         <Task />
       </div>
     </ApolloProvider>
